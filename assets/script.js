7018313bfb90eb96c4bcb641d2832fb0ddf0b0c2
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2017-12-17 13:55:41 -0500

    It begins

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+const fredRogers = $(	`<div class="col-md-3">
+							<div id="rogers" class="friendly character">
+						    	 <p>Fred Rogers</p>
+						     	<img class="smol">
+						    	 <p>180</p>
+						   	 </div>
+						 </div>  	 `);
+
+$(document).ready(function(){
+	console.log("whoomp");
+	$(fredRogers).appendTo("#conflicts");
+});
+
+// Choose your fighter 
+	//Map all the fighters  
+
+
+// Make enemies
+
+// Doin a fight
+	// Fight time
+	// Fight reslove
+	// Doin an animate
+
+//Restart function
